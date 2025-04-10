e83d79e234342f3bf4a9a741550ce557b2c681b1
Igor Lerinc <igor.lerinc7@gmail.com>
2025-04-10 16:06:02 +0200

    Phonebook backend step 3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,45 +1,58 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const PORT = 3002
+const PORT = 3002;
 
 let phonebook = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-app.get('/info', (req, res) => {
+app.get("/info", (req, res) => {
+  const date = new Date();
 
-    const date = new Date();
+  res.send(
+    `<p>Phonebook has info for ${
+      phonebook.length
+    } people</p><p>${new Date()}</p>`
+  );
+});
 
+app.get("/api/persons", (req, res) => {
+  res.json(phonebook);
+});
 
-    res.send(`<p>Phonebook has info for ${phonebook.length} people</p><p>${new Date()}</p>`)
-  })
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const contact = phonebook.find((contact) => contact.id === id);
 
-  
+  if (contact) {
+    res.json(contact);
+  } else {
+    res.status(404).send("Not found")
+  }
 
-app.get('/api/persons', (req,res) => {
-    res.json(phonebook)
-})
+ 
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
