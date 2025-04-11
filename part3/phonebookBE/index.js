6fb755e8aebcd9975008db497103c20f4f444786
Igor Lerinc <igor.lerinc7@gmail.com>
2025-04-11 13:01:24 +0200

    d

--- part3/phonebookBE/index.js
+++ part3/phonebookBE/index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3002;
 
 app.use(cors())
-app.use(express.static('dist'))
+
 
 morgan.token("post-data", (req) => {
   return req.method === "POST" ? JSON.stringify(req.body) : "";
