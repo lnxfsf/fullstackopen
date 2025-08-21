458676be38933784a589128bf975c35322ac4fdb
Igor Lerinc <igor.lerinc7@gmail.com>
2025-08-21 21:36:35 +0200

    4.15 & 4.16

--- part4/BlogList/models/Blog.js
+++ part4/BlogList/models/Blog.js
@@ -1,17 +1,5 @@
 const mongoose = require("mongoose");
 
-const config = require("../utils/config");
-
-const password = config.MONGODB_PASS;
-const database = config.MONGODB_DATABASE;
-console.log("database", database)
-
-const mongoUrl = `mongodb+srv://igorlerinc7:${password}@cluster0.n3bexs7.mongodb.net/${database}?retryWrites=true&w=majority&appName=Cluster0`;
-
-mongoose.connect(mongoUrl).then(()=> {
-  console.log("connected to mongodb")
-});
-
 const blogSchema = mongoose.Schema({
   title: String,
   author: String,
