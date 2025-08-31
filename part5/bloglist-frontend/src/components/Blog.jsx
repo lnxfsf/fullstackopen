897e24004b9fb67596d4f9de0f1d9dc5def4ba22
Igor Lerinc <igor.lerinc7@gmail.com>
2025-08-31 16:05:04 +0200

    5.9

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  const [visible, setVisible] = useState(false);
+
+  const showWhenVisible = { display: visible ? "" : "none" };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <>
+      <div style={blogStyle}>
+        {blog.title} | {blog.author}{" "}
+        <button onClick={toggleVisibility}>view</button>
+        <div style={showWhenVisible}>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes} <button>like</button>
+          </div>
+          <div>{blog?.user?.name}</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Blog;
