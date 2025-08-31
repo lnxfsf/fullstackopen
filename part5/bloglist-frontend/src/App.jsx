e88163531dfd3e292d0981c71a01ea8f426ca82a
Igor Lerinc <igor.lerinc7@gmail.com>
2025-08-31 16:30:46 +0200

    5.10

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -141,7 +141,7 @@ const App = () => {
           </div>
 
           <h2>blogs</h2>
-          {blogs.map((blog) => (
+          {blogs.sort((a, b) => b.likes - a.likes).map((blog) => (
             <Blog 
               key={blog.id}
               blog={blog}
