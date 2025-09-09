b77e5ab38a30ceadf16f7d9bc188883f6e68cf15
Igor Lerinc <igor.lerinc7@gmail.com>
2025-09-09 22:38:58 +0200

    7.4: Custom Hook for Form Fields

--- part7/routed-anecdotes/src/components/CreateNew.jsx
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -1,24 +1,32 @@
 import { useState } from 'react'
 import { useNavigate } from "react-router-dom";
+import  { useField } from '../hooks'
 
 
 const CreateNew = ({addNew, setNotification}) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+
+  // const [content, setContent] = useState('')
+  //const [author, setAuthor] = useState('')
+  //const [info, setInfo] = useState('')
+
   const navigate = useNavigate();
+
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
+
     addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
-    setNotification(`a new anecdote ${content} created!`)
+    setNotification(`a new anecdote ${content.value} created!`)
     setTimeout(() => {
       setNotification('')
     }, 5000)
@@ -31,15 +39,15 @@ const CreateNew = ({addNew, setNotification}) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name='content' {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input name='author' {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input name='info' {...info} />
         </div>
         <button>create</button>
       </form>
